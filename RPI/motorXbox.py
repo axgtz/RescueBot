c21e87b883ef2c849fc64b662dd4173332c843a8
axgtz <axguti@gmail.com>
2017-12-07 16:34:22 -0600

    back

--- RPI/motorXbox.py
+++ RPI/motorXbox.py
@@ -54,7 +54,7 @@ while not joy.Back():
             GPIO.output(Motor1B,GPIO.HIGH)
             pwm1 = GPIO.PWM(Motor1E,1000)
             pwm1.start(0)
-            print 'changed'
+            print 'changed=================================================================================='
         pwm1.ChangeDutyCycle(abs(ljoy * 100))
     # Left analog stick
     print "Ly ",ljoy,
