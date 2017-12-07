f95d62ea9284b6a978dd81fd6c21de3915d8ca3d
axgtz <axguti@gmail.com>
2017-12-07 15:06:18 -0600

    added motor left to work on joystick Y

--- RPI/motorXbox.py
+++ RPI/motorXbox.py
@@ -24,19 +24,45 @@ def fmtFloat(n):
 
 joy = xbox.Joystick()
 
+flag = 1
+ljoy = 0
+rjoy = 0
+
 print "Xbox controller sample: Press Back button to exit"
 # Loop until back button is pressed
 while not joy.Back():
+    ljoy = fmtFloat(joy.leftY())
+    rjoy = fmtFloat(joy.rightY())
     # Show connection status
     if joy.connected():
         print "Connected   ",
     else:
         print "Disconnected",
-
+    #Left Motor , motor number 1
+    if ljoy > 0:
+        if flag:
+            flag = 0
+            GPIO.output(Motor1A,GPIO.HIGH)
+            GPIO.output(Motor1B,GPIO.LOW)
+            pwm1 = GPIO.PWM(Motor1E,1000)
+            pwm1.start(0)
+        pwm1.ChangeDutyCycle(abs(ljoy * 100))
+    elif ljoy < 0:
+        if not flag:
+            flag = 1
+            GPIO.output(Motor1A,GPIO.LOW)
+            GPIO.output(Motor1B,GPIO.HIGH)
+            pwm1 = GPIO.PWM(Motor1E,1000)
+            pwm1.start(0)
+        pwm1.ChangeDutyCycle(abs(ljoy * 100))
     # Left analog stick
-    print "Ly ",fmtFloat(joy.leftY()),
+    print "Ly ",ljoy,
+
+    #Right Motor , motor number 2
+
+
     # Right analog stick
-    print "Ry ",fmtFloat(joy.rightY()),
+    print "Ry ",rjoy,
 
     # Move cursor back to start of line
     print chr(13),
@@ -58,7 +84,7 @@ joy.close()
 
 
 
-
+#
 #
 #
 # while(key != "q"):
@@ -99,6 +125,3 @@ joy.close()
 #                 GPIO.output(Motor1A,GPIO.LOW)
 #                 GPIO.output(Motor2B,GPIO.LOW)
 #                 GPIO.output(Motor2A,GPIO.LOW)
-#                 key = raw_input()
-#         else:
-#                 key = raw_input()
