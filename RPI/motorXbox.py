ceae9cc1fd76f36baf5829e90c12f73069cb14db
axgtz <axguti@gmail.com>
2017-12-07 16:31:18 -0600

    nada

--- RPI/motorXbox.py
+++ RPI/motorXbox.py
@@ -18,8 +18,6 @@ GPIO.setup(Motor1B,GPIO.OUT)
 GPIO.setup(Motor2A,GPIO.OUT)
 GPIO.setup(Motor2B,GPIO.OUT)
 
-pwm1 = GPIO.PWM(Motor1E,1000)
-
 # Format floating point number to string format -x.xxx
 def fmtFloat(n):
     return '{:6.3f}'.format(n)
@@ -46,7 +44,9 @@ while not joy.Back():
             flag = 0
             GPIO.output(Motor1A,GPIO.HIGH)
             GPIO.output(Motor1B,GPIO.LOW)
+            pwm1 = GPIO.PWM(Motor1E,1000)
             pwm1.start(0)
+            print 'front'
         pwm1.ChangeDutyCycle(abs(ljoy * 100))
     elif ljoy < 0:
         if not flag:
@@ -55,7 +55,7 @@ while not joy.Back():
             GPIO.output(Motor1B,GPIO.HIGH)
             pwm1 = GPIO.PWM(Motor1E,1000)
             pwm1.start(0)
-            print 'changed'
+            print 'back'
         pwm1.ChangeDutyCycle(abs(ljoy * 100))
     # Left analog stick
     print "Ly ",ljoy,
