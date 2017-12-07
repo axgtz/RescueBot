13e9efc901c25b43673e06ac81b5d1c9821b523d
axgtz <axguti@gmail.com>
2017-12-07 15:12:55 -0600

    negative for backwards

--- RPI/motorXbox.py
+++ RPI/motorXbox.py
@@ -54,8 +54,7 @@ while not joy.Back():
             GPIO.output(Motor1A,GPIO.LOW)
             pwm1 = GPIO.PWM(Motor1E,1000)
             pwm1.start(0)
-            print 'changed=================================================================================='
-        pwm1.ChangeDutyCycle(abs(ljoy * 100))
+        pwm1.ChangeDutyCycle(ljoy * 100)
     # Left analog stick
     print "Ly ",ljoy,
 
