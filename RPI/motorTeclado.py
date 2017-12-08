53ada04737a851109256e08e62e3f2b5a61de49a
axgtz <axguti@gmail.com>
2017-12-08 15:30:55 -0600

    added steam controller repo

--- RPI/motorTeclado.py
+++ RPI/motorTeclado.py
@@ -27,8 +27,12 @@ while(key != "q"):
             print "Forward"
             GPIO.output(Motor1A,GPIO.HIGH)
             GPIO.output(Motor1B,GPIO.LOW)
+            GPIO.output(Motor2A,GPIO.HIGH)
+            GPIO.output(Motor2B,GPIO.LOW)
             pwm1 = GPIO.PWM(Motor1E,1000)
+            pwm2 = GPIO.PWM(Motor2E,1000)
             pwm1.start(90)
+            pwm2.start(90)
 
             key = raw_input()
         elif(key == "s"):
